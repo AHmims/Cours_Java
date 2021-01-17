3fac6251e1f177696a113c8494977e8434b6ceb3
AHmims <ali.hmims99@gmail.com>
2021-01-17 20:39:39 +0100

    changes

--- les_classes/src/com/company/Main.java
+++ les_classes/src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Test objet = new Test();
+        System.out.println(objet.variable_1);
     }
 }
--- les_classes/src/com/company/Test.java
+++ les_classes/src/com/company/Test.java
@@ -3,6 +3,9 @@ package com.company;
 public class Test {
     String variable_1;
     String variable_2;
+    static String variable_3;
+    //
+    public Test(){}
     //
     public void method_1(){}
 }
