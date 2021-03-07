7bd40cdaed048fdd71a75578d4a448e973748317
AHmims <ali.hmims99@gmail.com>
2021-03-07 16:51:06 +0100

    calc

--- hello_world/src/com/company/Main.java
+++ hello_world/src/com/company/Main.java
@@ -1,8 +1,72 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    private static double first = 0;
+    private static double second = 0;
+    private static String op;
+    private static String temp = "";
+    //
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        temp = "5";
+        //temp += "3";
+        add();
+        temp = "5";
+        //temp += "0";
+        multiply();
+        temp = "2";
+        compute();
+        System.out.println(first);
+    }
+
+    //
+    private static void add() {
+        operation("+");
+    }
+
+    private static void multiply() {
+        operation("*");
+    }
+
+    //.....
+    //
+    //
+    private static void operation(String str) {
+        if(!op.equals(""))
+            calc();
+        op = str;
+        calc();
+    }
+
+    //
+    private static void calc() {
+        second = Double.parseDouble(temp);
+        temp = "";
+        switch (op) {
+            case "+":
+                first += second;
+                break;
+            case "-":
+                first -= second;
+                break;
+            case "*":
+                first *= second;
+                break;
+            case "/":
+                first /= second;
+                break;
+            default:
+                System.out.println("error");
+        }
+        //
+        System.out.println(first);
+        second = 0;
+    }
+
+    //
+    //
+    private static void compute() {
+        calc();
     }
 }
